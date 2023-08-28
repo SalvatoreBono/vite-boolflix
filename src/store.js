532d6754dad2000039078a6d845e38ea7afaad15
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-28 15:06:15 +0200

    try

--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
+import axios from "axios";
 import { reactive } from "vue";
 
 export const store = reactive({
     userTextFilm: "",
+    title: "",
 })
 
 export function searchFilm() {
-    console.log(this.store.userTextFilm)
+    const url = "https://api.themoviedb.org/3/search/movie"
+    axios.get(url, {
+        params: {
+            api_key: `e18bd326b1f37c5ea1e097b2f4c62927`,
+            query: `${store.userTextFilm}`,
+        }
+    })
+        .then((response) => {
+            store.title = response.data.results.original_title;
+        })
 }
