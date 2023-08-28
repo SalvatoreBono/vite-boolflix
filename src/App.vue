cc7c41b0064a5891b7f9ae9c33b43dc08ad03628
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-28 12:46:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+</template>
+
+<style lang="scss"></style>
