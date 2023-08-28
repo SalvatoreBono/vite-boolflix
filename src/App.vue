c812493d4ffcd184d3f982f51bd5f243d7aa4ee9
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-28 13:55:04 +0200

    add TheMain components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
 
 export default {
   components: {
     TheHeader,
+    TheMain,
   },
 };
 </script>
 
 <template>
   <TheHeader></TheHeader>
+  <TheMain></TheMain>
 </template>
 
 <style lang="scss"></style>
